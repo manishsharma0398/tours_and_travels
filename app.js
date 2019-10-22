c895ee8e79d395e81a3f0c35b15943c7e5f42a84
Manish Sharma <manish.sharma0398@gmail.com>
2019-10-22 09:31:21 +0530

    basic auth layout for tours and travel company

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// dotenv
+require("dotenv/config");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const authRoutes = require("./routes/auth/auth");
+app.use("/auth", authRoutes);
+
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected To DB!")
+);
 
 app.listen(3000);
---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+});
+
+module.exports = mongoose.model("users", UserSchema);
